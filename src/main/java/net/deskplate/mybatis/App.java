cbb51dce85bdec2a99dfbbd7186ed53d9f12f90a
katafuchix <katafuchix@gmail.com>
2022-07-03 18:52:21 +0900

    CRUD のサンプル

--- src/main/java/net/deskplate/mybatis/App.java
+++ src/main/java/net/deskplate/mybatis/App.java
@@ -2,6 +2,7 @@ package net.deskplate.mybatis;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.*;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -35,8 +36,33 @@ public class App
                 Integer count = map.insert(employee);
                 System.out.println(count.toString());
                 session.commit();
+
+                // 一覧取得
+                List<Employee> list = map.selectByExample(new EmployeeExample());
+                for (Employee s : list) {
+                    System.out.println(s.getId());
+                    System.out.println(s.getName());
+                    System.out.println(s.getRole());
+                }
+
+                Employee e1 = map.selectByPrimaryKey(1);
+                System.out.println(e1.getId());
+                System.out.println(e1.getName());
+                System.out.println(e1.getRole());
+
+                e1.setRole("CTO");
+
+                map.updateByPrimaryKeySelective(e1);
+                session.commit();
+
+                System.out.println(e1.getId());
+                System.out.println(e1.getName());
+                System.out.println(e1.getRole());
+
+                map.deleteByPrimaryKey(1);
+                session.commit();
+
                 session.close();
-                
             }
         } catch (IOException e) {
             System.out.println(e.toString());
@@ -44,3 +70,6 @@ public class App
         }
     }
 }
+
+// こんなふうにすると便利
+// https://bushansirgur.in/a-complete-crud-application-with-spring-mvc-and-mybatis-ibatis/
